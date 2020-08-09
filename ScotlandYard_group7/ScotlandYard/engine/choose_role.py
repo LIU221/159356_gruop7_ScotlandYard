0034288ea7ee1b74e0cecb53e3fb6af1666e388e
WangMinghao-19023259 <13363070186@163.com>
2020-08-09 17:48:38 +0800

    Add role selection page

---
+++ ScotlandYard_group7/ScotlandYard/engine/choose_role.py
@@ -0,0 +1,36 @@
+from tkinter import Tk, Label, IntVar, Button, Radiobutton
+
+from ScotlandYard.engine import mrx, detectives
+from ScotlandYard.engine.game import Game
+from ScotlandYard.engine.gui import Window
+
+
+class RolePage(Tk):
+    def __init__(self, mode, player_number):
+        Tk.__init__(self)
+
+        self.mode = mode
+        self.player_number = player_number
+
+        self.title('Choose role')
+
+        self.geometry('500x300')
+        self.label_choose_your_game_role = Label(self, text="Choose your game role:")
+
+        self.label_choose_your_game_role.pack(fill='x')
+
+        roles = ['X', 'A', 'B', 'C', 'D', 'E']
+
+        self.v = IntVar()
+        for i in range(player_number):
+            rb = self.rb1 = Radiobutton(self, text=roles[i], variable=self.v, value=i)
+            rb.pack()
+
+        self.button_confirm = Button(self, text="Confirm", command=self.confirm)
+        self.button_confirm.pack()
+
+    def confirm(self):
+        self.destroy()
+        the_game = Game(mrx, detectives, self.player_number)
+        win = Window(the_game, self.mode, self.player_number, self.v.get())
+        win.mainloop()
