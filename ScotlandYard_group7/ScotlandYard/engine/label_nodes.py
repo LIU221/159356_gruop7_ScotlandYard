5c0f088a5ac0f33cc2765fe59d8ccf0fbe7f9286
ShiYiming0 <55150892+ShiYiming0@users.noreply.github.com>
2020-08-09 10:19:12 +0800

    Update label_nodes.py
    label_node is done

---
+++ ScotlandYard_group7/ScotlandYard/engine/label_nodes.py
@@ -0,0 +1,26 @@
+from tkinter import Tk, Canvas
+from PIL import ImageTk, Image
+import os
+
+root = Tk()
+cnv = Canvas(root, width=2000, height=2000)
+baseDirectory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+fileName = baseDirectory + os.sep + "Files" + os.sep + "board.jpg"
+img = Image.open(fileName)
+img = img.resize((2000, 2000))
+img = ImageTk.PhotoImage(image=img)
+cnv.create_image(1000, 1000, image=img)
+n = 1
+
+
+def callback(evt):
+    global n
+    print(n, evt.x, evt.y)
+    n += 1
+
+
+cnv.bind("<Button-1>", callback)
+
+cnv.pack()
+
+root.mainloop()
