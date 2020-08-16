0516574883c51149b2452da34f4cfe8c293e4b43
LIU221 <1021226989@qq.com>
2020-08-16 16:17:51 +0800

    Django

---
+++ ScotlandYard_group7/ScotlandYard_group7/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ScotlandYard_group7 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ScotlandYard_group7.settings')
+
+application = get_wsgi_application()
