393940a23ecda1ca7269200140b346662954dc39
WangMinghao-19023259 <13363070186@163.com>
2020-10-21 22:22:29 +0800

    Add the users files for server

---
+++ server/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.login),
+    path('register/', views.register),
+    path('login/', views.login)
+    # path('', )  # views.index, name='index'),
+]
