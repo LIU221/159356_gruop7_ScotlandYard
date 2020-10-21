8b2ee6858748ab9134b4684f591d4ea830bfa660
L-Z-M <55149340+L-Z-M@users.noreply.github.com>
2020-10-22 00:09:32 +0800

    Add files via upload

---
+++ server/game/routing.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+
+from . import consumers
+from . import consumersx
+
+websocket_urlpatterns = [
+    re_path(r'ws/gamex', consumersx.ChatConsumerx),
+    re_path(r'ws/game', consumers.ChatConsumer),
+    
+]
